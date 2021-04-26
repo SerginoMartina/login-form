06a202f51180df579e4dae0c65b03a1c6ae3dc09
sergino <sergino.martina@ida-mediafoundry.nl>
2021-04-26 15:56:41 +0200

    more users are possible now

--- objects.js
+++ objects.js
@@ -1,10 +1,17 @@
-var userOne = {
-    email: 'userOne@gmail.com',
-    name: 'Sergino',
-    login(){
-        console.log(this.name, 'has logged in');
-    },
-    logout(){
-        console.log(this.name, 'has logged out');
-    }
+function User(email, name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+}
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.name, 'has logged in');
 };
+
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.name, 'has logged out');
+};
+
+var userOne = new User('sergino.martina@gmail.com', 'sergino');
